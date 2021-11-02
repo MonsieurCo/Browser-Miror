6efda98e8b2c0afe528eeb1d6f32606d798fd153
MonsieurCo <48797321+MonsieurCo@users.noreply.github.com>
2021-11-02 17:46:10 +0100

    small code to show rtequest of the navigator( not a final code)

---
+++ miror.py
@@ -0,0 +1,24 @@
+import socket
+
+
+def server():
+    HOST = '127.0.0.1'
+    PORT = 2003  # random port above 1024
+    MAXBYTES = 4096
+    print(HOST, PORT)
+    server_socket = socket.socket()
+
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    while True:
+        conn, address = server_socket.accept()
+        print("Connection from: " + str(address))
+        data = conn.recv(MAXBYTES).decode("utf8")
+        if not data:
+            break
+        print("ECHO RESQUEST: \n",data,sep="")
+    conn.close()  # close the connection
+
+
+if __name__ == '__main__':
+    server()
