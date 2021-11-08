ee18e9e968a873c56de091cd7c63d312bd8634dd
MonsieurCo <48797321+MonsieurCo@users.noreply.github.com>
2021-11-08 22:07:50 +0100

    soem stuf added but nothing really works

--- miror.py
+++ miror.py
@@ -1,4 +1,5 @@
 import socket
+from urllib.parse import urlparse
 '''
 To run this code I put a proxy on my computer at 127.0.0.1:2003 
 
@@ -16,10 +17,36 @@ def server():
     while True:
         conn, address = server_socket.accept()
         print("Connection from: " + str(address))
-        data = conn.recv(MAXBYTES).decode("utf8")
+        rawdata = conn.recv(MAXBYTES)
+        data = rawdata.decode("utf8")
         if not data:
             break
-        print("ECHO RESQUEST: \n",data,sep="")
+        print("ECHO RESQUEST: \n",(rawdata+b"\r\n\r\n").decode("utf8"),sep="")
+        parsed = data.split(" ")
+        method = parsed[0]
+        h = parsed[2]
+        URL = parsed[1]
+        u = urlparse(URL)
+        print(URL)
+        if u.scheme == '':
+            addressTO = (u.geturl().split(":")[0], int(u.geturl().split(":")[1]))
+        else:
+            addressTO = (u.geturl(),8080)
+        print(addressTO)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(addressTO)
+        #res = [method,h].append(data[2:])
+        #datas = " ".join(parsed[2:])
+        #print(datas)
+        s.sendall(rawdata+b"\r\n\r\n")
+        new_data = s.recv(4096)
+        while(len(new_data)< 1):
+            new_data = s.recv(4096)
+        s.close()
+        print("RECIEVED : ")
+        print(new_data.decode("utf8"))
+        conn.send(new_data)
+
     conn.close()  # close the connection
 
 if __name__ == '__main__':
